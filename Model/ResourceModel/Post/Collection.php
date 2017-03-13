b838065a6ba40ee1a7bbeace9d191f7442119fe4
Alex Wang <selltrends@gmail.com>
2017-03-13 09:56:35 +0800

    add models & resource models

---
+++ Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,21 @@
+<?php
+namespace Atopt\Blog\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'post_id';
+
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Atopt\Blog\Model\Post', 'Atopt\Blog\Model\ResourceModel\Post');
+    }
+
+}
