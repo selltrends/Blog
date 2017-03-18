6b9d1288f014286928b5a9b92f3af09f38b48ecf
Alex Wang <selltrends@gmail.com>
2017-03-18 13:46:17 +0800

    frontend controllers, blocks and templates

---
+++ Block/PostView.php
@@ -0,0 +1,55 @@
+<?php
+namespace Atopt\Blog\Block;
+
+class PostView extends \Magento\Framework\View\Element\Template implements
+    \Magento\Framework\DataObject\IdentityInterface
+{
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Atopt\Blog\Model\Post $post
+     * @param \Atopt\Blog\Model\PostFactory $postFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Atopt\Blog\Model\Post $post,
+        \Atopt\Blog\Model\PostFactory $postFactory,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->_post = $post;
+        $this->_postFactory = $postFactory;
+    }
+    /**
+     * @return \Atopt\Blog\Model\Post
+     */
+    public function getPost()
+    {
+        // Check if posts has already been defined
+        // makes our block nice and re-usable! We could
+        // pass the 'posts' data to this block, with a collection
+        // that has been filtered differently!
+        if (!$this->hasData('post')) {
+            if ($this->getPostId()) {
+                /** @var \Atopt\Blog\Model\Post $page */
+                $post = $this->_postFactory->create();
+            } else {
+                $post = $this->_post;
+            }
+            $this->setData('post', $post);
+        }
+        return $this->getData('post');
+    }
+    /**
+     * Return identifiers for produced content
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        return [\Atopt\Blog\Model\Post::CACHE_TAG . '_' . $this->getPost()->getId()];
+    }
+}
